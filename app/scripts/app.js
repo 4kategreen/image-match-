195b676f2624d850f726c53adc2856cac68c2702
Kate Green <kate.green@chronicle.com>
2014-04-18 15:17:01 -0400

    Adding Auth service and stuff to allow sign in.

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,15 +5,30 @@ angular.module('babiesApp', [
   'ngResource',
   'ngSanitize',
   'ngRoute',
-  'lvl.directives.dragdrop'
+  'lvl.directives.dragdrop',
+  'firebase'
 ])
+  .constant('FIREBASE_URL', 'https://4kategreen.firebaseio.com')
+  .constant('USERS', 370373795)
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        controller: 'MainCtrl',
       })
       .otherwise({
         redirectTo: '/'
       });
-  });
+  })
+  .run(['$rootScope', 'Auth', '$location', function($rootScope, Auth, $location) {
+    $rootScope.loginObj = Auth;
+
+    $rootScope.$on('$routeChangeStart', function(event, currRoute){
+      // also create a test that it's not me.
+      // reload redirects
+      if (currRoute.$$route.authRequired && !$rootScope.loginObj.user) {
+        // make a modal or something here.
+        $location.path('/');
+      }
+    });
+  }]);
